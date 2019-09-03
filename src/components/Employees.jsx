1f9bf52e082273e90dcea168e13ed37ec9a54399
Andy Garcia <v-andgarcia@expedia.com>
2019-09-03 14:03:04 -0700

    Working subscriptions, just not locally

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -5,37 +5,72 @@ import subscriptions from '../graphql/subscriptions';
 import EmployeeOverview from './EmployeeOverview';
 import EmployeeInput from './EmployeeInput'
 import client from '../utils/AppSyncClient';
+import { API, graphqlOperation } from 'aws-amplify';
+
 
 class Employees extends Component {
+    employeeSubscription = null;
+
     state = {
-        employees: []
+        employees: [],
+        employeesWaiting: []
     }
 
     componentDidMount() {
-        client.query({
-            query: queries.listEmployees
-        }).then(({ data: { listEmployees } }) => {
-            console.log(listEmployees);
-            this.setState({ employees: listEmployees.items }, this.subscribeToNewEmployees)
-        }).catch((err) => {
-            console.error(err);
-            this.setState({ result: err })
-        })
+        // client.query({
+        //     query: queries.listEmployees
+        // }).then(({ data: { listEmployees } }) => {
+        //     console.log(listEmployees);
+        //     this.setState({ employees: listEmployees.items }, this.subscribeToNewEmployees)
+        // }).catch((err) => {
+        //     console.error(err);
+        //     this.setState({ result: err })
+        // })
+        API.graphql(graphqlOperation(queries.listEmployees))
+            .then(({ data: { listEmployees: { items: employees } } }) => {
+                this.setState({ employees: employees }, this.subscribeToNewEmployees)
+            })
+
+    }
+
+    componentWillUnmount() {
+        this.employeeSubscription && this.employeeSubscription.unsubscribe();
     }
 
     subscribeToNewEmployees = async () => {
         // return;
-        const result = await client.subscribe({
-            query: subscriptions.newEmployee
-        }).subscribe({
-            next: data => {
-                console.log(data)
-            },
-            error: error => {
-                console.error(error);
+        // const result = await client.subscribe({
+        //     query: subscriptions.newEmployee
+        // }).subscribe({
+        //     next: data => {
+        //         console.log("Retrieving data", data)
+        //     },
+        //     error: error => {
+        //         console.error("Error in subscription", error);
+        //     }
+        // })
+        // console.log("subscription reuslt", result);
+        this.employeeSubscription = await API.graphql(
+            graphqlOperation(subscriptions.newEmployee)
+        ).subscribe({
+            next: (data) => {
+                console.log("Incoming data", data)
+                const addedEmployee = data.value.data.newEmployee;
+                console.log("Waiting employee", addedEmployee);
+                this.setState({
+                    employees: [...this.state.employees, addedEmployee],
+                })
             }
+        });
+    }
+
+    loadNewEmployees = async () => {
+        this.setState({
+            employees: [
+                ...this.state.employees,
+                ...this.state.employeesWaiting
+            ]
         })
-        console.log("subscription reuslt", result);
     }
 
     getSortedEmployees = () => {
@@ -55,6 +90,9 @@ class Employees extends Component {
             <React.Fragment>
                 <h1>All Employees (Live Updates)</h1>
                 <EmployeeInput />
+                {
+                    this.state.employeesWaiting.length > 0 && <button onClick={this.loadNewEmployees}>New Employees Available... Load Now</button>
+                }
                 {employees.length > 0 ?
                     employees.map(({ id, name, role }) =>
                         (<EmployeeOverview key={id} id={id} name={name} role={role} />))
